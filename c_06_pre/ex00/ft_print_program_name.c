79620d8371e5ce727ca43f80b53983178627aec0
Teo Jimenez <teojimen@cbr8s4.42barcelona.com>
2023-07-27 17:48:13 +0200

    6

---
+++ c_06_pre/ex00/ft_print_program_name.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: teojimen <teojimen@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 14:49:18 by teojimen          #+#    #+#             */
+/*   Updated: 2023/07/27 15:01:52 by teojimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_strprint(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+		ft_strprint(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
---
+++ c_06_pre/ex01/ft_print_params.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: teojimen <teojimen@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 15:03:50 by teojimen          #+#    #+#             */
+/*   Updated: 2023/07/27 15:13:55 by teojimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_strprint(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_strprint(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
